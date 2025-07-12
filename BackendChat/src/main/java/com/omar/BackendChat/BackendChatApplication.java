896bd22e5bf1f50b22d8824aaf4fbc54a3783a23
OmarSaez <omar.saez@usach.cl>
2025-07-12 19:23:14 -0400

    Se agrega la base del backend con dependencias en springboot

---
+++ BackendChat/src/main/java/com/omar/BackendChat/BackendChatApplication.java
@@ -0,0 +1,13 @@
+package com.omar.BackendChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChatApplication.class, args);
+	}
+
+}
