0e215dcff868a506ea4a0a3ae145593732fde68a
OmarSaez <omar.saez@usach.cl>
2025-07-13 12:05:41 -0400

    Se arma archivo para crear la base de datos en PostgreSQL

---
+++ Tablas.sql
@@ -0,0 +1,65 @@
+-- COmando en CMD: psql -U tu_usuario -d postgres -f Tablas.sql
+
+-- Crear la base de datos
+CREATE DATABASE BasedatosChat;
+
+-- Conectarse a la base (solo funciona desde psql CLI)
+\c BasedatosChat;
+
+
+--Copiar desde aqui si es por medio de Query en pgAdmin
+-- Crear tablas en orden
+
+CREATE TABLE Usuario (
+    ID SERIAL PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Email VARCHAR(150) UNIQUE,
+    Contrasena VARCHAR(50),
+    Rol INT
+);
+
+CREATE TABLE Chat (
+    ID SERIAL PRIMARY KEY
+);
+
+CREATE TABLE Grupo (
+    ID SERIAL PRIMARY KEY,
+    Nombre VARCHAR(30),
+    ID_Chat INT UNIQUE,
+    FOREIGN KEY (ID_Chat) REFERENCES Chat(ID)
+);
+
+CREATE TABLE Mensaje (
+    ID SERIAL PRIMARY KEY,
+    ID_Usuario INT,
+    ID_Chat INT,
+    Contenido TEXT NOT NULL,
+    FechaEnvio TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID),
+    FOREIGN KEY (ID_Chat) REFERENCES Chat(ID)
+);
+
+CREATE TABLE UsuarioChat (
+    ID_Usuario INT,
+    ID_Chat INT,
+    PRIMARY KEY (ID_Usuario, ID_Chat),
+    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID),
+    FOREIGN KEY (ID_Chat) REFERENCES Chat(ID)
+);
+
+CREATE TABLE MiembroGrupo (
+    ID_Usuario INT,
+    ID_Grupo INT,
+    Admin BOOLEAN,
+    PRIMARY KEY (ID_Usuario, ID_Grupo),
+    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID),
+    FOREIGN KEY (ID_Grupo) REFERENCES Grupo(ID)
+);
+
+CREATE TABLE Contactos (
+    ID_Usuario INT,
+    ID_Contacto INT,
+    PRIMARY KEY (ID_Usuario, ID_Contacto),
+    FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID),
+    FOREIGN KEY (ID_Contacto) REFERENCES Usuario(ID)
+);
